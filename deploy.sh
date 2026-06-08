9871573e9e9220a820543aed556c3a50b9ecf958
David Albarran <david@elitelabs.es>
2026-06-08 20:32:41 +0000

    fix: remove secrets from repo, add to gitignore

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd /var/www/elitelabs
-git checkout -- package.json package-lock.json
-git pull origin main
-npm install
-cp -r public .next/standalone/public
-pm2 restart elitelabs --update-env
-echo "Deploy completado $(date)"
